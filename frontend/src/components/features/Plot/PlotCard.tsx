3543b29ccd515c35cf48b8c300ee3264d721ea07
temLv <alyahov49@gmail.com>
2026-04-23 17:17:44 +0300

    feat: изменил отступы в карточке и добавил ссылку

--- frontend/src/components/features/Plot/PlotCard.tsx
+++ frontend/src/components/features/Plot/PlotCard.tsx
@@ -2,27 +2,30 @@ import {Card} from "@heroui/react";
 import {LucideSquarePen, LucideTrash2} from "lucide-react";
 
 interface Props {
+  id: string;
   title: string;
   createdAt: string;
   photo?: string;
 };
 
-export function PlotCard({title, createdAt, photo}: Props) {
+export function PlotCard({id, title, createdAt, photo}: Props) {
   return (
     <Card className="p-0 w-fit relative transition duration-300 hover:scale-101">
-      <div className="max-w-120 max-h-60 overflow-hidden">
-        <img src={photo || "/plot3.avif"} alt={title} className="w-full" />
-      </div>
-      <div className="flex items-center justify-between w-full px-5 pb-5">
-        <div className='flex flex-col items-start justify-between'>
-          <h3>{title}</h3>
-          <span>Создан: {createdAt}</span>
+      <a href={`${id}`}>
+        <div className="max-w-120 max-h-60 overflow-hidden">
+          <img src={photo || "/plot3.avif"} alt={title} className="w-full" />
         </div>
-        <div className="flex gap-3.5">
-          <button><LucideSquarePen/></button>
-          <button className="text-red-600"><LucideTrash2/></button>
+        <div className="flex items-center justify-between w-full px-5 py-3">
+          <div className='flex flex-col items-start justify-between'>
+            <h3>{title}</h3>
+            <span>Создан: {createdAt}</span>
+          </div>
+          <div className="flex gap-3.5">
+            <button><LucideSquarePen/></button>
+            <button className="text-red-600"><LucideTrash2/></button>
+          </div>
         </div>
-      </div>
+      </a>
     </Card>
   );
 };

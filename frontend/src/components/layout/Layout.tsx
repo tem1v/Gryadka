2a9a672682ab1ef1bf6436136b8f6ecda6d1481e
temLv <alyahov49@gmail.com>
2026-04-25 21:14:23 +0300

    style:Поднял кнопку открытия сайдбара, и поместил ее в карточку

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -2,6 +2,7 @@ import {type PropsWithChildren, useState} from "react";
 import {Sidebar} from "./sidebar/Sidebar.tsx";
 import {PanelLeft} from "lucide-react";
 import {cn} from "@heroui/styles";
+import { Card } from "@heroui/react";
 
 export function Layout({children}:PropsWithChildren) {
   const [isOpen, setIsOpen] = useState(true);
@@ -12,12 +13,13 @@ export function Layout({children}:PropsWithChildren) {
     isOpen={isOpen}
     onToggle={() => setIsOpen(!isOpen)}
   />
-    <button
-      onClick={() => setIsOpen(true)}
-      className={cn("mt-2.5 duration-300 absolute top-10 left-10", isOpen ? "opacity-0 pointer-events-none" : "opacity-100")}
-    >
-      <PanelLeft className='text-primary hover:opacity-80 duration-300 cursor-pointer' size={30} strokeWidth={2}/>
-    </button>
+    <Card className={cn("flex items-end mt-2 duration-300 absolute top-1 -left-5 w-25 py-2 z-10", isOpen ? "-translate-x-full pointer-events-none" : "opacity-100")}>
+      <button
+        onClick={() => setIsOpen(true)}
+      >
+        <PanelLeft className='text-primary hover:opacity-80 duration-300 cursor-pointer' size={30} strokeWidth={2}/>
+      </button>
+    </Card>
     <main className='flex-1 p-2 overflow-y-auto '>
       {children}
     </main>

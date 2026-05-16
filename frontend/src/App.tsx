e50641e708e635fade9d20d434de7a4722aceee3
temLv <alyahov49@gmail.com>
2026-05-16 16:11:51 +0300

    feat:Реализована страница растения

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import {WeatherPage} from "@/pages/WeatherPage.tsx";
 import {InventoryPage} from "@/pages/InventoryPage.tsx";
 import {TasksPage} from "@/pages/TasksPage.tsx";
 import {PlotPage} from "@/pages/PlotPage.tsx";
+import {PlantPage} from "@/pages/PlantPage.tsx";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/plots" element={<PlotsPage/>}/>
         <Route path="/plots/:id" element={<PlotPage/>}/>
+        <Route path="/plots/:id/plant/:id" element={<PlantPage/>}/>
         <Route path="/weather" element={<WeatherPage/>}/>
         <Route path="/tasks" element={<TasksPage/>}/>
         <Route path="/inventory" element={<InventoryPage/>}/>
